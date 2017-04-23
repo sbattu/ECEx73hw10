98ac8211031d20ea4ef8bb619bd47b775fa3b720
swakin reddy battu <swakin reddy battu>
2017-04-22 19:08:25 -0500

    added new server name and url

--- https_example.py
+++ https_example.py
@@ -7,10 +7,10 @@ context.check_hostname = True
 context.load_default_certs()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssl_sock = context.wrap_socket(s, server_hostname='help.github.com')
+ssl_sock = context.wrap_socket(s, server_hostname='www.google.com')
 
 try:
-    ssl_sock.connect(('help.github.com', 443))
+    ssl_sock.connect(('www.google.com.com', 443))
     ssl_sock.settimeout(1.0)
     ssl_sock.sendall("GET /articles/which-remote-url-should-i-use/ HTTP/1.1\r\nHostname: help.github.com\r\n\r\n")
     while 1:
